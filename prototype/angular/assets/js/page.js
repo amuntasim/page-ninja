47af9c8cf8a6fe9b4ccb4bc5f061c9bf15638d89
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2012-12-20 11:15:59 +0600

    Added editable directives

--- prototype/angular/assets/js/page.js
+++ prototype/angular/assets/js/page.js
@@ -1,33 +1,44 @@
-//var app = angular.module('editable', []);
-//
-//app.directive('inlineEdit', function factory(injectables) {
-//    var directiveDefinitionObject = {
-//        template: '<div></div>',
-//        restrict: 'E',
-//        link: function postLink(scope, iElement, iAttrs) { ... }
-//    };
-//    return directiveDefinitionObject;
-//});
+var app = angular.module('editable', []);
 
-function removeItem(array, item) {
-    for (var i in array) {
-        if (array[i] == item) {
-            array.splice(i, 1);
-            break;
-        }
-    }
-}
+app.directive('ngEnter', function() {
+    return function(scope, elm, attrs) {
+        elm.bind('keypress', function(e) {
+            if (e.charCode === 13) scope.$apply(attrs.ngEnter);
+        });
+    };
+});
+
+app.directive('editableString', function factory() {
+    return {
+        template:'<span ng-hide="editMode" ng-dblclick="editMode=true">{{prop}}</span>' +
+                 '<span ng-show="editMode">' +
+                    '<input ng-enter="editMode=false" type="text" ng-model="prop"/><a herf="#" ng-click="editMode=false">done</a>' +
+                 '</span>',
+        restrict:'E',
+        scope: {prop: '=bind'}
+    };
+});
+app.directive('editableText', function factory() {
+    return {
+        template:'<span ng-hide="editMode" ng-dblclick="editMode=true">{{prop}}</span>' +
+                 '<span ng-show="editMode">' +
+                    '<textarea ng-model="prop"></textarea><a herf="#" ng-click="editMode=false">done</a>' +
+                 '</span>',
+        restrict:'E',
+        scope: {prop: '=bind'}
+    };
+});
 
 function PageCtrl($scope) {
     $scope.page = {title:'Hello, world!',
-        dialog:'This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.',
+        dialog:'This is a template for a simple marketing or informational website.',
         hero_button_text:'Learn more »',
         portfolios:[
             {title:'Heading',
-                description:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+                description:'Donec',
                 button_text:'View details »'},
             {title:'Heading',
-                description:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+                description:'Donec',
                 button_text:'View details »'}
         ]};
 

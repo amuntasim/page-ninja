798800c1c73a4dc4580116f958345ca41e04146d
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-01-21 12:55:11 +0600

    Add new and remove is working with data binding

--- prototype/angular/assets/js/page.js
+++ prototype/angular/assets/js/page.js
@@ -1,17 +1,17 @@
 var app = angular.module('editable', []);
 
 var editableStringTemplate = '<span ng-hide="editMode" ng-dblclick="editMode=true">{{prop}}</span>' +
-                             '<span ng-show="editMode">' +
-                                '<input ng-enter="editMode=false" type="text" ng-model="prop"/><a herf="#" ng-click="editMode=false">done</a>' +
-                             '</span>';
+    '<span ng-show="editMode">' +
+    '<input ng-enter="editMode=false" type="text" ng-model="prop"/>' +
+    '</span>';
 var editableTextTemplate = '<span ng-hide="editMode" ng-dblclick="editMode=true">{{prop}}</span>' +
-                           '<span ng-show="editMode">' +
-                               '<textarea ng-model="prop"></textarea><a herf="#" ng-click="editMode=false">done</a>' +
-                           '</span>';
+    '<span ng-show="editMode">' +
+    '<textarea ng-model="prop"></textarea><a herf="#" ng-click="editMode=false">done</a>' +
+    '</span>';
 
-app.directive('ngEnter', function() {
-    return function(scope, elm, attrs) {
-        elm.bind('keypress', function(e) {
+app.directive('ngEnter', function () {
+    return function (scope, elm, attrs) {
+        elm.bind('keypress', function (e) {
             if (e.charCode === 13) scope.$apply(attrs.ngEnter);
         });
     };
@@ -19,16 +19,16 @@ app.directive('ngEnter', function() {
 
 app.directive('editableString', function factory() {
     return {
-        template: editableStringTemplate,
+        template:editableStringTemplate,
         restrict:'AE',
-        scope: {prop: '=bind'}
+        scope:{prop:'=bind'}
     };
 });
 app.directive('editableText', function factory() {
     return {
-        template: editableTextTemplate,
+        template:editableTextTemplate,
         restrict:'AE',
-        scope: {prop: '=bind'}
+        scope:{prop:'=bind'}
     };
 });
 
@@ -44,7 +44,9 @@ function PageCtrl($scope) {
     $scope.page = {title:'Hello, world!',
         dialog:'This is a template for a simple marketing or informational website.',
         hero_button_text:'Learn more »',
-        menu:[{text: 'Home', link: '#'}],
+        menu:[
+            {text:'Home', link:'#'}
+        ],
         portfolios:[
             {title:'Heading',
                 description:'Donec',
@@ -54,13 +56,20 @@ function PageCtrl($scope) {
                 button_text:'View details »'}
         ]};
 
-    $scope.addMore = function (exp) {
-        var nestedVar = exp.split('.');
-        var scope = $scope;
-        for(var i=0 ; i < nestedVar.length ; i++) {
-            scope = scope[nestedVar[i]];
-        }
-        var newClonedObject = JSON.parse(JSON.stringify(scope[scope.length - 1]));
-        scope.push(newClonedObject);
+    $scope.addMore = function (parentObj) {
+        var newClonedObject = JSON.parse(JSON.stringify(parentObj[parentObj.length - 1]));
+        parentObj.push(newClonedObject);
     };
+    $scope.remove = function (portfolios, portfolio) {
+        removeItem(portfolios, portfolio);
+    };
+}
+
+function removeItem(array, item) {
+    for (var i in array) {
+        if (array[i] == item) {
+            array.splice(i, 1);
+            break;
+        }
+    }
 }
